895f9c76d225f657a673580fc62f210b7994c981
aevix <55609333+aevix@users.noreply.github.com>
2020-03-30 20:14:29 -0500

    functioning 60 second timer

--- script.js
+++ script.js
@@ -1,24 +1,12 @@
-var canvas = document.getElementsByClassName("myCanvas");
-var ctx = canvas.getContext("2d");
-
-var button = document.getElementsByClassName("reset");
-
-var refresh = function() {
-
-ctx.clearRect(0,0,400,400);
-for(i=0; i<600; i++) {
-
-var x = Math.floor(Math.random()*800);
-var y = Math.floor(Math.random()*480);
-var radius = Math.floor(Math.random()*30);
-  
-ctx.beginPath();
-ctx.arc(x,y,radius,Math.PI*2,0,false);
-ctx.fill();
-ctx.closePath();
-  
+function countdown() {
+    var time = setInterval(helper,1000);
+    function helper() {
+        var sec = document.getElementById("seconds").textContent;
+        sec--;
+        document.getElementById("seconds").textContent = sec;
+        if (document.getElementById("seconds").textContent == 0) {
+            document.getElementById("button").textContent = "Reset"
+            clearInterval(time);
+        }
+    }
 }
-}
-
-refresh();
-button.addEventListener("click", refresh, false);
