a3d55209e566a9edad2a29c30bd6a1523ca84a39
aevix <55609333+aevix@users.noreply.github.com>
2020-03-31 00:23:19 -0500

    fix timer bug and adjust UI

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 function countdown() {
     var time = setInterval(helper,1000);
+    if (document.getElementById("seconds").textContent > 0 && 
+    document.getElementById("seconds").textContent < 60){
+        clearInterval(time);
+    }
     function helper() {
         var sec = document.getElementById("seconds").textContent;
         sec--;
         document.getElementById("seconds").textContent = sec;
         if (document.getElementById("seconds").textContent == 0) {
-            document.getElementById("button").textContent = "Reset"
+            document.getElementById("button").textContent = "Reset";
             clearInterval(time);
         }
+    }
+
+    if (document.getElementById("button").textContent == "Reset"){
+        document.getElementById("seconds").textContent = 60;
+        document.getElementById("button").textContent = "Start";
     }
 }
