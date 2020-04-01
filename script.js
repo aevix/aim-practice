98b0dbdfb905f0c7ff56edf5fe93417f2361586c
aevix <55609333+aevix@users.noreply.github.com>
2020-04-01 17:43:01 -0500

    update UI add submit button for text box

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 function countdown() {
-    var time = setInterval(helper,1000);
     if (document.getElementById("seconds").textContent > 0 && 
     document.getElementById("seconds").textContent < 60){
         clearInterval(time);
+    } else {
+        var time = setInterval(helper,1000);
     }
     function helper() {
         var sec = document.getElementById("seconds").textContent;
@@ -19,3 +20,12 @@ function countdown() {
         document.getElementById("button").textContent = "Start";
     }
 }
+
+function myFunction() {
+    var c = document.getElementById("myCanvas");
+    var ctx = c.getContext("2d");
+    ctx.beginPath();
+    ctx.arc(100, 75, 50, 0, 2 * Math.PI);
+    
+    document.canvas.appendChild(c);
+  }
