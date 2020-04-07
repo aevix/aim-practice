e514a6a65e1ffb815f8ecca8be4cf65996dd084e
aevix <55609333+aevix@users.noreply.github.com>
2020-04-07 02:42:42 -0500

    fix button glitch
    removed pause and resume function and fixed the glitch where if you hit the start button multiple times before the timer start counting down it would spawn targets really fast.

--- script.js
+++ script.js
@@ -1,32 +1,50 @@
+
+
 function countdown() {
-    if (document.getElementById("button").textContent == "Pause"){
-        clearInterval(time);
-    } else if(document.getElementById("button").textContent == "Reset"){
-        document.getElementById("seconds").textContent = 60;
+    if (document.getElementById("button").textContent == "Reset") {
         document.getElementById("button").textContent = "Start";
+        document.getElementById("seconds").textContent = 60;
     } else {
-        var time = setInterval(helper,1000);
-        document.getElementById("button").textContent = "Pause"
-    }
-    function helper() {
-        var sec = document.getElementById("seconds").textContent;
-        sec--;
-        document.getElementById("seconds").textContent = sec;
-        if (document.getElementById("seconds").textContent == 0) {
-            document.getElementById("button").textContent = "Reset";
-            clearInterval(time);
+        document.getElementById("button").textContent = "Reset";
+        var time = setInterval(helper, 1000);
+        function helper() {
+            if (document.getElementById("seconds").textContent == 0) {
+                clearInterval(time);
+            } else if (document.getElementById("button").textContent == "Start") {
+                clearInterval(time);
+            } else {
+                var sec = document.getElementById("seconds").textContent;
+                sec--;
+                document.getElementById("seconds").textContent = sec;
+            }
         }
     }
 }
 
+
 function randomNum(start,end) {
     return Math.floor(Math.random()*end) + start;
 }
+
+var mouse = {
+    x: undefined,
+    y: undefined
+}
+var canvas = document.getElementById('myCanvas');
+
+canvas.addEventListener('click',
+    function (event) {
+        mouse.x = event.x;
+        mouse.y = event.y;
+        console.log(mouse);
+    }
+)
+
 function targetGenerator() {
-    var target = setInterval(randomCircles, 750);
+    var target = setInterval(randomCircles, 900);
+    var c = document.getElementById('myCanvas');
+    var ctx = c.getContext('2d');
     function randomCircles() {
-        var c = document.getElementById('myCanvas');
-        var ctx = c.getContext('2d');
         ctx.clearRect(0,0,800, 480);
         var x = randomNum(40,760);
         var y = randomNum(40,440);
@@ -35,8 +53,10 @@ function targetGenerator() {
         ctx.arc(x,y,r,0*Math.PI,2*Math.PI);
         ctx.fillStyle = "white";
         ctx.fill();
-        if (document.getElementById("seconds").textContent == 0) {
+        if (document.getElementById("seconds").textContent == 0 || document.getElementById("button").textContent == "Start") {
             clearInterval(target);
-      }
+            ctx.clearRect(0,0,800,480);
+        }
     }
 }
+
