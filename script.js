795d9fbcea6acb6a622c624f5f464d580eb72ae2
aevix <55609333+aevix@users.noreply.github.com>
2020-04-12 02:51:49 -0500

    add score by even listener

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ function countdown() {
     if (document.getElementById("button").textContent == "Reset") {
         document.getElementById("button").textContent = "Start";
         document.getElementById("seconds").textContent = 60;
+        document.getElementById("points").textContent = 0;
     } else {
         var time = setInterval(helper, 1000);
         document.getElementById("button").textContent = "Reset";
@@ -23,6 +24,9 @@ function countdown() {
     }
 }
 
+var x = undefined;
+var y = undefined;
+var r = undefined;
 
 function randomNum(start,end) {
     return Math.floor(Math.random()*end) + start;
@@ -33,10 +37,10 @@ function targetGenerator() {
     var c = document.getElementById('myCanvas');
     var ctx = c.getContext('2d');
     function randomCircles() {
+        x = randomNum(40,760);
+        y = randomNum(40,440);
+        r = randomNum(10,15);
         ctx.clearRect(0,0,800, 480);
-        var x = randomNum(40,760);
-        var y = randomNum(40,440);
-        var r = randomNum(10,15);
         ctx.beginPath();
         ctx.arc(x,y,r,0*Math.PI,2*Math.PI);
         ctx.fillStyle = "white";
@@ -48,22 +52,42 @@ function targetGenerator() {
     }
 }
 
-function startTracking() {
-    var mouse = {
-        x: undefined,
-        y: undefined
-    }
-    
-    const myCanvas = document.getElementById('myCanvas');
-    const context = myCanvas.getContext('2d');
-    const offset = myCanvas.getBoundingClientRect();
-    if (document.getElementById("button").textContent == "Reset") {
-        myCanvas.addEventListener('click',
-            function (event) {
-                mouse.x = event.x - offset.left;
-                mouse.y = event.y - offset.top;
-                console.log(mouse);
-            }
-        ) 
-    }
+function getMousePos(canvas, evt) {
+    var rect = canvas.getBoundingClientRect();
+    return {
+      x: evt.clientX - rect.left,
+      y: evt.clientY - rect.top
+    };
 }
+
+function addCanvas() {
+    var canvas = document.getElementById('myCanvas');
+    var context = canvas.getContext('2d');
+    canvas.addEventListener('click', function(evt) {
+        var mousePos = getMousePos(canvas, evt);
+        if (mousePos.x >= x-r && mousePos.x <= x+r && mousePos.y >= y-r && mousePos.y <= y+r) {
+            var points = document.getElementById("points").textContent;
+            points ++;
+            document.getElementById("points").textContent = points;
+        }
+        }, false);
+}
+
+
+// var mouse = {
+//     x: undefined,
+//     y: undefined
+// }
+
+// function mouseTracking() { 
+//     var canvas = document.getElementById('myCanvas');
+//     var context = canvas.getContext('2d');
+//     const offset = canvas.getBoundingClientRect();
+//     canvas.addEventListener('click',
+//         function (event) {
+//             mouse.x = event.pageX - offset.left,
+//             mouse.y = event.pageY - offset.top
+//             console.log(mouse);
+//         }
+//     )
+// }
