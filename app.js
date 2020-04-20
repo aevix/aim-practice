f67ff5080171d86f4517d34eac0101ba70978641
aevix <55609333+aevix@users.noreply.github.com>
2020-04-19 23:49:49 -0500

    css score board layout change.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
+var data = [{name: "yuhyun", score: 100}, {name: "eddie", score: 96}, {name: "april", score: 87}];
 
 var urlencodedParser = bodyParser.urlencoded({ extended: false});
 
@@ -8,7 +9,7 @@ app.set('view engine', 'ejs');
 app.use('/assets', express.static('assets'));
 
 app.get('/', function(req, res){
-  res.render("profile");
+  res.render("profile", {topplayer: data});
 });
 
 app.post('/', urlencodedParser, function(req, res){
